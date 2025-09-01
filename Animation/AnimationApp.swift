7715a7e86605826d04875bcb4b325e2aee4d4317
aref <devfera14@gmail.com>
2025-09-01 12:25:14 -0300

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by arefdeveloper on 01/09/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
